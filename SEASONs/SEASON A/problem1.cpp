6c992729e46a2dd3b992be80019cc96fc50cdd82
M VISHWAS KAMATH <vishwask.cs18@sahyadri.edu.in>
2020-05-20 10:57:46 +0530

    Update problem1.cpp

---
+++ SEASONs/SEASON A/problem1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, b, a[100000];
+
+void solve() {
+	cin >> n >> b;
+	for(int i=0; i<n; ++i)
+	{
+		cin >> a[i];
+	}
+	sort(a, a+n);
+	int ans=0;
+	for(int i=0; i<n; i++)
+	{
+		if(b>=a[i])
+		{
+			b-=a[i];
+			++ans;
+		}
+	}
+	cout << ans << "\n";
+}
+
+int main() {
+	int t, i=1;
+	cin >> t;
+	while(t--) {
+		cout << "Case #" << i << ": ";
+		solve();
+		++i;
+	}
+}
